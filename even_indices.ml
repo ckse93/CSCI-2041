d0c8fa23e4bc41ee4447163465acc2d0c9e3e3fd
Chan Jung <ckse93@gmail.com>
2018-12-06 20:30:55 -0600

    Update even_indices.ml

--- even_indices.ml
+++ even_indices.ml
@@ -1,12 +1,31 @@
-(*take a list as an input, returns even indices of that list only*)
-
 let lst = [0;1;2;3;4;5;6;7;8;9];;
 
-let rec even_indicies in_list=
+let rec even_list_indicies in_list=
   match in_list with
   | [] -> []
   | head :: [] -> [head]
   | head :: middle :: tail -> head :: even_indicies tail
   ;;
 
+let rec odd_list_indicies in_list =
+  match in_list with
+  | [] -> []
+  | head :: [] -> []
+  | even :: odd :: tail -> odd :: odd_indicies tail
+;;
   even_indicies lst;;
+  odd_indicies lst;;
+
+let hi_order_sum_list in_list =
+  List.fold_left (+) 0 in_list
+;;
+
+let sum_list in_list =
+  let rec helper init lst =
+    match lst with
+    | [] -> init
+    | head :: tail -> helper (init + List.hd lst) (List.tl lst)
+  in
+  helper 0 in_list
+;;
+sum_list lst;;
