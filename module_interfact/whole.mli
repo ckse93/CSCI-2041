cae09a1b54238cfedba33dd0cc3f3425c3bbf41e
Chan Jung <ckse93@gmail.com>
2018-12-12 09:48:44 -0600

    Create whole.mli
    interface file that restricts what can be seen and what cannot be seen. 
    Everything that is included here will be visible to other .ml file that tries to use contents in whole.ml file. This file will restrict access to any element in whole.ml file if they are not listed in this file

---
+++ module_interfact/whole.mli
@@ -0,0 +1,5 @@
+val str : string;;
+val thelist : int list;;
+val show_thelist : unit -> int list;;
+val show_str : unit -> string;;
+(*giving access to everything except for you_can_see_me stuffs*)
