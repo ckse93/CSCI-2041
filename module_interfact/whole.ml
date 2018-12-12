6935cd9bf449a9b06ba448c6ecc1619c35bd1c2e
Chan Jung <ckse93@gmail.com>
2018-12-12 09:46:27 -0600

    Create whole.ml
    master file that contains everything.

---
+++ module_interfact/whole.ml
@@ -0,0 +1,12 @@
+let thelist = [1;2;3;4;5;6];;
+let str = "Hello :)\n";;
+let you_cant_see_me ="JOOOOOOOOOHN CEEEEEEEEEEENAAAAAAAAA";;
+
+let show_thelist() =
+thelist;;
+
+let show_str() =
+str;;
+
+let show_you_cant_see_me =
+you_cant_see_me;;
